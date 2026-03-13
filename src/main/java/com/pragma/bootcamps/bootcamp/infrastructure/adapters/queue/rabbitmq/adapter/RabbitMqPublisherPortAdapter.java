380eec7e2e62f5125f97fde0e37316394b09603b
georffrey.arevalo <georffrey.arevalo@pragma.com.co>
2026-03-13 04:01:50 -0500

    feat(adapters): Environment Variables of RabbitMQ Configured

--- src/main/java/com/pragma/bootcamps/bootcamp/infrastructure/adapters/queue/rabbitmq/adapter/RabbitMqPublisherPortAdapter.java
+++ src/main/java/com/pragma/bootcamps/bootcamp/infrastructure/adapters/queue/rabbitmq/adapter/RabbitMqPublisherPortAdapter.java
@@ -3,13 +3,11 @@ package com.pragma.bootcamps.bootcamp.infrastructure.adapters.queue.rabbitmq.ada
 import com.pragma.bootcamps.bootcamp.domain.queue.BootcampMessage;
 import com.pragma.bootcamps.bootcamp.domain.queue.port.QueuePublisherPort;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
-import tools.jackson.databind.ObjectMapper;
 
 @Component
 @RequiredArgsConstructor
@@ -17,7 +15,6 @@ public class RabbitMqPublisherPortAdapter implements QueuePublisherPort {
 
     private final Queue queue;
     private final RabbitTemplate rabbitTemplate;
-    private final ObjectMapper objectMapper;
 
     @Override
     public Mono<Void> sendBootcampReportMessage(BootcampMessage message) {
