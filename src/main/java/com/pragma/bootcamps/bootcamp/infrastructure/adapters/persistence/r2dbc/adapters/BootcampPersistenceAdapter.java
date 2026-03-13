370c8089b14e7282212e61d4707f392c3de221bf
georffrey.arevalo <georffrey.arevalo@pragma.com.co>
2026-03-12 23:51:20 -0500

    feat(adapters): Adapters of bootcamp persistence

--- src/main/java/com/pragma/bootcamps/bootcamp/infrastructure/adapters/persistence/r2dbc/adapters/BootcampPersistenceAdapter.java
+++ src/main/java/com/pragma/bootcamps/bootcamp/infrastructure/adapters/persistence/r2dbc/adapters/BootcampPersistenceAdapter.java
@@ -6,9 +6,14 @@ import com.pragma.bootcamps.bootcamp.domain.spi.BootcampPersistencePort;
 import com.pragma.bootcamps.bootcamp.infrastructure.adapters.persistence.r2dbc.mappers.BootcampEntityMapper;
 import com.pragma.bootcamps.bootcamp.infrastructure.adapters.persistence.r2dbc.repositories.BootcampReactiveRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BootcampPersistenceAdapter implements BootcampPersistencePort {
@@ -31,5 +36,16 @@ public class BootcampPersistenceAdapter implements BootcampPersistencePort {
     public Mono<Bootcamp> findBootcampByName(String name) {
         return bootcampReactiveRepository.findByName(name)
                 .map(mapper::toDomain);
+    }
+
+    @Override
+    public Flux<Bootcamp> findBootcampsPagedAndSorted(int page, int size, String sortBy, String order) {
+        return Mono.just(order)
+                .map(ord -> ord.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC)
+                .map(direction -> Sort.by(direction, sortBy))
+                .map(sort -> PageRequest.of(page, size, sort))
+                .flatMapMany(bootcampReactiveRepository::findAllBy)
+                .map(mapper::toDomain)
+                .doOnNext(boc -> log.info("[DB RESULT] bootcamp_id={}, name={}, capabilityCount={}", boc.getId(), boc.getName(), boc.getCapabilityCount()));
     }
 }
--- src/main/java/com/pragma/bootcamps/bootcamp/infrastructure/adapters/persistence/r2dbc/repositories/BootcampReactiveRepository.java
+++ src/main/java/com/pragma/bootcamps/bootcamp/infrastructure/adapters/persistence/r2dbc/repositories/BootcampReactiveRepository.java
@@ -1,11 +1,14 @@
 package com.pragma.bootcamps.bootcamp.infrastructure.adapters.persistence.r2dbc.repositories;
 
 import com.pragma.bootcamps.bootcamp.infrastructure.adapters.persistence.r2dbc.entities.BootcampEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface BootcampReactiveRepository extends ReactiveCrudRepository<BootcampEntity, Long> {
 
     Mono<BootcampEntity> findByName(String name);
+    Flux<BootcampEntity> findAllBy(Pageable pageable);
 
 }
